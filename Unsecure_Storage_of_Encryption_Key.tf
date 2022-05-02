2043e4b4e856c50acb5c189a3e9f1eaac42d2770
Satyam Chaurasia <satyam.chaurasia@checkmarx.com>
2022-05-02 16:06:15 +0530

    Satyam

--- Unsecure_Storage_of_Encryption_Key.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "S3B_541"
-  acl    = "public-read"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
--- s3.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "S3B_181355"
-  acl    = "public-read"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
